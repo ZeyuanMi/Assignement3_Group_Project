b279dca85f459ea4dc55692b7810f1378135a59e
Zeyuan Mi <3225525769@qq.com>
2026-05-06 15:42:46 +0100

    Fix a relative path problem

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import pool from '@/lib/db';
+import pool from '../../../../lib/db';
 import { comparePassword, generateToken } from '../../../../lib/auth';
 
 export async function POST(req) {
