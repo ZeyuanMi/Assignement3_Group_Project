3964be2308179b287e17899276f2c75930ab5db9
shichong8634 <2057646043@qq.com>
2026-05-10 05:28:05 +0100

    Yunlong: update page.js (Complete the homepage interface) Add a title and a 'login button'
    /2026/5/10-5:28

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
+    return(
+    <div style={{ textAlign: "center", marginTop: "50px",border: "30px solid gray"}}>
 
+        <h1>Flight Booking</h1>
+
+        <div style={{ marginTop: "30px" }}>
+            <a href="/login" style={{ margin: "20px", padding: "10px 20px", background: "red", color: "white", textDecoration: "none", borderRadius: "5px",fontFamily: "Georgia" }}>
+                Part A: Cinema Booking
+            </a>
+        </div>
+
+    </div>
+);
 }
