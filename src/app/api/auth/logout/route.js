b7a8b04a4464b8fcfe4059804315189a9402a05d
shichong8634 <2057646043@qq.com>
2026-05-10 04:59:04 +0100

    Yunlong: Add logout API
    /2026/5/10-4:58

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -2,4 +2,19 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function POST() {
+
+    try {
+
+        // Delete session cookie
+        cookies().delete("user");
+
+        return NextResponse.json(
+            {msg: "Logout successful"},
+            {status: 200}
+        );
+
+    }
+
+
+
 }
