da4e268a2e21665ad62ac54a891b76651f78b568
shichong8634 <2057646043@qq.com>
2026-05-10 04:41:38 +0100

    Yunlong:Create a MySQL database connection pool/2026/5/10-4:42

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,1 +1,10 @@
 import mysql from "mysql2/promise";
+
+//Create a MySQL database connection pool
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "flight_booking",
+});
