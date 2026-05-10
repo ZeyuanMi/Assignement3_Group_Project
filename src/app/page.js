ec4f4f0d0da7ce29b8dc0b03b6d676ded83389d1
shichong8634 <2057646043@qq.com>
2026-05-10 05:30:32 +0100

    Yunlong: update page.js (Complete the homepage interface) fix some mistakes background color change to white
    /2026/5/10-5:30

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
     return(
-    <div style={{ textAlign: "center", marginTop: "50px",border: "30px solid gray"}}>
+    <div style={{ textAlign: "center", marginTop: "50px",border: "10px solid gray",backgroundColor: "white"}}>
 
         <h1>Flight Booking</h1>
 
         <div style={{ marginTop: "30px" }}>
-            <a href="/login" style={{ margin: "20px", padding: "10px 20px", background: "red", color: "white", textDecoration: "none", borderRadius: "5px",fontFamily: "Georgia" }}>
-                Part A: Cinema Booking
+            <a href="/login" style={{ margin: "40px", padding: "10px 20px", background: "red", color: "white", textDecoration: "none", borderRadius: "5px",fontFamily: "Georgia" }}>
+                Login
             </a>
         </div>
 
